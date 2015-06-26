134ac4d84bda96e14ff6372979fdc82c8d198b6d
Angelo Olivera <aolivera@gmail.com>
2015-06-26 18:10:25 -0400

    restructure code into Backup class

--- bin/chef-backup.rb
+++ bin/chef-backup.rb
@@ -5,16 +5,10 @@ $:.unshift(File.join(execpath, "lib"))
 
 require 'yaml'
 require 'logger'
-require 'optparse'
 require 'ostruct'
+require 'optparse'
 
-require 'git'
-require 'repo'
-require 'util'
-require 'chef/knife'
-require 'chef/api_client'
-require 'chef/user'
-require 'chef/node'
+require 'chef-backup'
 
 config = YAML.load_file(File.join(execpath, 'config', 'chef-backup.yml'))['chef-backup']
 
@@ -30,6 +24,7 @@ end
 @logger.level = config['loglevel'] ? Logger.const_get(config['loglevel']) : Logger::INFO
 
 @options = OpenStruct.new
+@options.logger = @logger
 @options.verbose = false
 @options.path = config['path'] ? config['path'] : "#{ENV['PWD']}/backup"
 
@@ -45,76 +40,5 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-Chef::Knife.new.configure_chef
-
-path = @options[:path]
-
-begin
-  r = Git::Base.open(path, {log: @logger})
-rescue ArgumentError
-  repo_url = config['repo_url']
-  unless repo_url
-    msg = 'local git repository does not exist, please specify repo_url in the config file'
-    @logger.fatal msg
-    STDERR.puts msg
-    exit 15
-  end
-
-  msg = "local git repository does not exist, cloning from `#{repo_url}' into `#{path}'"
-  @logger.warn msg
-  STDERR.puts msg
-
-  r = Git.clone(repo_url, path)
-rescue
-  msg = 'unable to create/clone repository'
-  @logger.fatal msg
-  STDERR.puts msg
-  STDERR.puts "Exception: #{$!}", $@
-  exit 10
-end
-
-# empty working tree
-r.clean
-
-# fetch objects
-
-## Nodes, roles, environments
-objects = {
-  nodes: Chef::Node,
-  users: Chef::User,
-  clients: Chef::ApiClient,
-  roles: Chef::Role,
-  environments: Chef::Environment,
-}
-
-objects.each do |type, klass|
-  STDERR.puts "Saving #{type}..."
-  klass.list.keys.each do |item|
-    data = api_request("#{klass}.load('#{item}')")
-    if klass == Chef::Node
-      delete_keys = [ 'last_successful_run' ]
-      data = {
-        "name" => data.name,
-        "chef_environment" => data.chef_environment,
-        "run_list" => data.primary_runlist.run_list.map { |k| k.to_s },
-        "normal" => data.attributes.normal.delete_if{ |k,_| delete_keys.include? k},
-      }
-    end
-    path = "#{@options['path']}/#{type}/#{item}.json"
-    save_file(path, data, @options[:verbose])
-  end
-end
-
-## Data bags
-STDERR.puts "Saving data bags..."
-api_request("Chef::DataBag.list").keys.each do |bag|
-  api_request("Chef::DataBag.load('#{bag}')").keys.each do |item|
-    path = "#{@options[:path]}/data_bags/#{bag}/#{item}.json"
-    data = api_request("Chef::DataBag.load('#{bag}/#{item}')")
-    save_file(path, data, @options[:verbose])
-  end
-end
-
-# update tree
-r.update_all
-
+b = ChefBackup::Backup.new(@options)
+puts b.run
--- lib/chef-backup.rb
+++ lib/chef-backup.rb
@@ -1,1 +1,141 @@
+require 'git'
+require 'repo'
+require 'util'
+require 'chef/knife'
+require 'chef/api_client'
+require 'chef/user'
+require 'chef/node'
+
 require 'chef-backup/version'
+
+module ChefBackup
+  ObjectTypes = {
+    users: Chef::User,
+    nodes: Chef::Node,
+    clients: Chef::ApiClient,
+    roles: Chef::Role,
+    environments: Chef::Environment,
+  }
+
+  class Backup
+    def initialize(options)
+      Chef::Knife.new.configure_chef
+
+      @options = options
+      @logger = options.logger
+    end
+
+    def nodes
+      self.common_object(__method__)
+    end
+
+    def users
+      self.common_object(__method__)
+    end
+
+    def clients
+      self.common_object(__method__)
+    end
+
+    def roles
+      self.common_object(__method__)
+    end
+
+    def environments
+      self.common_object(__method__)
+    end
+
+    def common_object(type)
+
+      klass = ObjectTypes[type.to_sym]
+      count = 0
+
+      STDERR.puts "Saving #{type}..."
+      klass.list.keys.each do |item|
+        data = api_request("#{klass}.load('#{item}')")
+        if klass == Chef::Node
+          delete_keys = [ 'last_successful_run' ]
+          data = {
+            "name" => data.name,
+            "chef_environment" => data.chef_environment,
+            "run_list" => data.primary_runlist.run_list.map { |k| k.to_s },
+            "normal" => data.attributes.normal.delete_if{ |k,_| delete_keys.include? k},
+          }
+        end
+        path = "#{@options['path']}/#{type}/#{item}.json"
+        save_file(path, data, @options[:verbose])
+        count += 1
+      end
+
+      count
+    end
+
+    def data_bags
+      count = 0
+      STDERR.puts "Saving data bags..."
+      api_request("Chef::DataBag.list").keys.each do |bag|
+        api_request("Chef::DataBag.load('#{bag}')").keys.each do |item|
+          path = "#{@options[:path]}/data_bags/#{bag}/#{item}.json"
+          data = api_request("Chef::DataBag.load('#{bag}/#{item}')")
+          save_file(path, data, @options[:verbose])
+          count += 1
+        end
+      end
+
+      count
+    end
+
+    def configure_repo
+      path = @options[:path]
+
+      begin
+        repo = Git::Base.open(path, {log: @logger})
+      rescue ArgumentError
+        repo_url = config['repo_url']
+        unless repo_url
+          msg = 'local git repository does not exist, please specify repo_url in the config file'
+          @logger.fatal msg
+          STDERR.puts msg
+          exit 15
+        end
+
+        msg = "local git repository does not exist, cloning from `#{repo_url}' into `#{path}'"
+        @logger.warn msg
+        STDERR.puts msg
+
+        repo = Git.clone(repo_url, path)
+      rescue
+        msg = 'unable to create/clone repository'
+        @logger.fatal msg
+        STDERR.puts msg
+        STDERR.puts "Exception: #{$!}", $@
+        exit 10
+      end
+
+      repo
+    end
+
+    def run
+
+
+      @repo = self.configure_repo
+      # empty wo\king tree
+      @repo.clean
+
+      # Back up all common types
+      ObjectTypes.keys.each do |type|
+        total = self.send(type)
+        @logger.info "total: #{total}"
+      end
+
+      # And data bags
+      self.data_bags
+
+      # update tree
+      r.update_all
+    end
+
+  end
+end
+
+
