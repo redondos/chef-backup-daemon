c665679a0905c2b9d5229160cd93a4958fa356e2
Angelo Olivera <aolivera@gmail.com>
2015-06-30 13:44:14 -0400

    daemonize process

--- bin/chef-backup.rb
+++ bin/chef-backup.rb
@@ -4,11 +4,8 @@ execpath = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 $:.unshift(File.join(execpath, "lib"))
 
 require 'yaml'
-require 'logger'
-require 'ostruct'
-require 'optparse'
-
-require 'chef-backup'
+require 'fileutils'
+require 'pathname'
 
 begin
   config = YAML.load_file(File.join(execpath, 'config', 'chef-backup.yml'))['chef-backup']
@@ -16,20 +13,12 @@ rescue Errno::ENOENT
   config = YAML.load_file(File.join('config', 'chef-backup.yml'))['chef-backup']
 end
 
-logfile = config['logfile'] ? config['logfile'] : File.join('log', File.basename(__FILE__) + '.log')
-
-begin
-  FileUtils.mkdir_p(File.dirname(logfile)) unless File.exists? File.dirname(logfile)
-  @logger = Logger.new logfile
-rescue Errno::EACCES
-  STDERR.puts "permission denied creating logfile #{logfile}"
-end
-
-@logger.level = config['loglevel'] ? Logger.const_get(config['loglevel']) : Logger::INFO
+require 'optparse'
+require 'ostruct'
 
 @options = OpenStruct.new
-@options.logger = @logger
 @options.verbose = false
+@options.daemon = false
 @options.path = config['path'] ? config['path'] : "#{ENV['PWD']}/backup"
 @options.repo_url = config['repo_url'] ? config['repo_url'] : nil
 @options.backup_frequency = config['frequency'] ? config['frequency'] : 30
@@ -46,14 +35,58 @@ OptionParser.new do |opts|
   opts.on('-p', '--path PATH', 'save files in PATH') do |path|
     @options[:path] = path
   end
+
+  opts.on('-d', '--daemon', 'daemonize') do |daemon|
+    @options[:daemon] = daemon
+  end
 end.parse!
 
-b = ChefBackup::Daemon.new(@options)
+logfile = config['logfile'] ? config['logfile'] : File.join('log', File.basename(__FILE__) + '.log')
+logfile = File.join(execpath, logfile) if ! (Pathname.new logfile).absolute?
+
+require 'daemons'
+require 'logger'
+
+daemon_opts = {
+  :ontop      => !@options[:daemon],
+  :backtrace  => false,
+  :log_output => false,
+}
+
+Daemons.daemonize(daemon_opts)
 
 begin
-  b.run
-rescue Interrupt
-  @logger.info "received interrupt, exiting"
+  FileUtils.mkdir_p(File.dirname(logfile)) unless File.exists? File.dirname(logfile)
+  @logger = Logger.new logfile
+rescue Errno::EACCES
+  STDERR.puts "permission denied creating logfile #{logfile}"
 end
 
+@logger.level = config['loglevel'] ? Logger.const_get(config['loglevel']) : Logger::INFO
+
+@options.logger = @logger
+@options.logfile = logfile
+
+mode = @options[:daemon] ? "daemonized" : "foreground"
+@logger.info "Starting chef-backup-daemon (#{mode}), backup path: #{@options['path']}" if @logger
+
+require 'chef-backup'
+
+backup = ChefBackup::Backup.new(@options)
+frequency = @options['backup_frequency']
+
+loop {
+  begin
+    backup.run
+  rescue Exception
+    @logger.fatal $!
+    @logger.fatal $@
+    STDERR.puts $!, $@
+  end
+
+  msg = "sleeping #{frequency}s"
+  @logger.info msg
+  STDERR.puts msg
+  sleep frequency
+}
 
--- lib/chef-backup.rb
+++ lib/chef-backup.rb
@@ -1,6 +1,5 @@
 require 'chef-backup/version'
 require 'chef-backup/backup'
-require 'chef-backup/daemon'
 
 module ChefBackup
 end
